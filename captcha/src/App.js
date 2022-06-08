fcfe073c97cf3b34fec77f97c9378b6e290fc7b1
gusfel <agustinjfeliciano@gmail.com>
2022-06-07 21:33:15 -0700

    got captcha working

---
+++ captcha/src/App.js
@@ -0,0 +1,92 @@
+/* eslint-disable no-alert */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+import React, { useState } from 'react';
+
+const photos = {
+  ONE:
+  'https://images.pexels.com/photos/2249528/pexels-photo-2249528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  TWO:
+  'https://images.pexels.com/photos/1061141/pexels-photo-1061141.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  THREE:
+  'https://images.pexels.com/photos/2249530/pexels-photo-2249530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  FOUR:
+  'https://images.pexels.com/photos/1061139/pexels-photo-1061139.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  FIVE:
+  'https://images.pexels.com/photos/1010973/pexels-photo-1010973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  SIX:
+  'https://images.pexels.com/photos/4772874/pexels-photo-4772874.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+};
+
+function App() {
+  return <Captcha />;
+}
+function Block({ number, success }) {
+  const topRow = ['ONE', 'TWO', 'THREE'];
+  const bottomRow = ['FOUR', 'FIVE', 'SIX'];
+
+  const handleClick = (e) => {
+    const { name } = e.target;
+    if (name === number) {
+      success();
+    } else {
+      alert('wrong one!');
+    }
+  };
+
+  const makeRow = (arr) => arr.map((val) => <input type="image" alt="" key={val} name={val} src={photos[val]} width="100" height="100" onClick={(e) => handleClick(e)} />);
+
+  return (
+    <div>
+      <div>
+        {makeRow(topRow)}
+      </div>
+      <div>
+        {makeRow(bottomRow)}
+      </div>
+    </div>
+  );
+}
+
+function Captcha() {
+  const [correct, setCorrect] = useState(true);
+  const [number, setNumber] = useState(0);
+  const numbers = {
+    1: 'ONE',
+    2: 'TWO',
+    3: 'THREE',
+    4: 'FOUR',
+    5: 'FIVE',
+    6: 'SIX',
+  };
+
+  const getNumber = () => {
+    setNumber(numbers[Math.floor(Math.random() * 6) + 1]);
+  };
+
+  const handleClick = async () => {
+    getNumber();
+    setCorrect(false);
+  };
+
+  const success = () => {
+    setCorrect(true);
+  };
+
+  return (
+    <div>
+      {correct
+        ? <button onClick={handleClick} type="button">show captcha</button>
+        : (
+          <div>
+            click
+            {' '}
+            {number}
+            <Block number={number} success={success} />
+          </div>
+        )}
+    </div>
+  );
+}
+
+export default App;
